c2ddfd7a79cf64e447026db35a03837ae0e5e820
Advaith H L <niceadvaith@gmail.com>
2022-04-16 22:57:32 +0530

    Display posts in feed

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from .models import Post
+
 
 @login_required
 def main_feed(request):
-    return render(request, 'feed/home.html')
+    posts = Post.objects.filter(creator=request.user.id)
+    return render(request, 'feed/home.html', {'posts': posts})
