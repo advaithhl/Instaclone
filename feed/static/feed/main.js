9364d836162f0ef7ce41f478d6c4531baf6a8771
Advaith H L <niceadvaith@gmail.com>
2022-04-26 14:11:11 +0530

    Show/hide post button

--- feed/static/feed/main.js
+++ feed/static/feed/main.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
-  $("textarea")
-    .each(function () {
-      this.setAttribute(
-        "style",
-        "height:" + this.scrollHeight + "px;overflow-y:hidden;"
-      );
-    })
-    .on("input", function () {
-      this.style.height = "auto";
-      this.style.height = this.scrollHeight + "px";
-    });
+  $("textarea").each(function () {
+    this.setAttribute(
+      "style",
+      "height:" + this.scrollHeight + "px;overflow-y:hidden;"
+    );
+  });
 
   $("textarea.comment-create-input").on({
-    focus: function () {
-      $("#post-button").show();
-    },
-    blur: function () {
+    "input propertychange": function () {
       $("#post-button").hide();
+      if (this.value.length) {
+        $("#post-button").show();
+        this.style.height = "auto";
+        this.style.height = this.scrollHeight + "px";
+      } else {
+        this.style.height = "38px";
+      }
     },
   });
 });
