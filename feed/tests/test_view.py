ea7ccb4027463a1daec43467aa9d4b37c90b034b
Advaith H L <niceadvaith@gmail.com>
2022-05-26 09:46:23 +0530

    Add more tests

--- feed/tests/test_view.py
+++ feed/tests/test_view.py
@@ -131,3 +131,124 @@ class TestCreatePostView:
         assert created_post.creator == request.user
         assert created_post.caption == caption
         assert created_post.liked_users.all().count() == 0
+
+
+@pytest.mark.django_db
+class TestEditPostView:
+    def test_anonymous(self):
+        request = RequestFactory().get(
+            reverse('instaclone-edit_post_view', kwargs={'pk': '1'}))
+        request.user = AnonymousUser()
+        response = views.edit_post_view(request)
+        logger.info(f'Redirect URL is {response.url}')
+        assert response.status_code == 302
+        assert response.url == f'{reverse(settings.LOGIN_URL)}?next={reverse("instaclone-edit_post_view", kwargs={"pk": "1"})}'
+
+    def test_edit_by_non_creator_get(self):
+        post = mixer.blend(Post)
+        logger.info(f'Mixer created post is {post}')
+        logger.info(f'Mixer created post has id {post.id}')
+        logger.info(f'Mixer created post has caption {post.caption}')
+        logger.info(f'Mixer created post has creator {post.creator}')
+        logger.debug(f'All posts queryset is {Post.objects.all()}')
+        non_creator = mixer.blend(User)
+        logger.info(f'Mixer created new user is {non_creator}')
+        request = RequestFactory().get(
+            reverse('instaclone-edit_post_view', kwargs={'pk': post.id}))
+        request.user = non_creator
+        response = views.edit_post_view(request, pk=post.id)
+        logger.info(f'Redirect URL is {response.url}')
+        assert response.status_code == 302
+        assert response.url == f'{reverse("instaclone-post_view", kwargs={"pk": post.id})}'
+
+    def test_edit_by_non_creator_post(self):
+        post = mixer.blend(Post)
+        logger.info(f'Mixer created post is {post}')
+        logger.info(f'Mixer created post has id {post.id}')
+        logger.info(f'Mixer created post has caption {post.caption}')
+        logger.info(f'Mixer created post has creator {post.creator}')
+        logger.debug(f'All posts queryset is {Post.objects.all()}')
+
+        original_caption = post.caption
+        new_caption = random_string(512)
+        logger.debug(
+            f'Randomly generated string being set as the new caption is {new_caption}')
+        data = {
+            'caption': new_caption,
+        }
+        request = RequestFactory().post(
+            reverse('instaclone-edit_post_view', kwargs={'pk': post.id}),
+            data=data,
+        )
+        non_creator = mixer.blend(User)
+        logger.info(f'Mixer created new user is {non_creator}')
+        request.user = non_creator
+        response = views.edit_post_view(request, pk=post.id)
+        assert response.status_code == 302
+        assert response.url == f'{reverse("instaclone-post_view", kwargs={"pk": post.id})}'
+        post.refresh_from_db()
+        assert post.caption != new_caption
+        assert post.caption == original_caption
+
+    def test_edit_by_creator_get(self):
+        post = mixer.blend(Post)
+        logger.info(f'Mixer created post is {post}')
+        logger.info(f'Mixer created post has id {post.id}')
+        logger.info(f'Mixer created post has caption {post.caption}')
+        logger.info(f'Mixer created post has creator {post.creator}')
+        logger.debug(f'All posts queryset is {Post.objects.all()}')
+        request = RequestFactory().get(
+            reverse('instaclone-edit_post_view', kwargs={'pk': post.id}))
+        request.user = post.creator
+        response = views.edit_post_view(request, pk=post.id)
+        assert response.status_code == 200
+        assert b'form' in response.content
+
+    def test_edit_by_creator_post(self):
+        post = mixer.blend(Post)
+        logger.info(f'Mixer created post is {post}')
+        logger.info(f'Mixer created post has id {post.id}')
+        logger.info(f'Mixer created post has caption {post.caption}')
+        logger.info(f'Mixer created post has creator {post.creator}')
+        logger.debug(f'All posts queryset is {Post.objects.all()}')
+
+        new_caption = random_string(512)
+        logger.debug(
+            f'Randomly generated string being set as the new caption is {new_caption}')
+        data = {
+            'caption': new_caption,
+        }
+        request = RequestFactory().post(
+            reverse('instaclone-edit_post_view', kwargs={'pk': post.id}),
+            data=data,
+        )
+        request.user = post.creator
+        response = views.edit_post_view(request, pk=post.id)
+        assert response.status_code == 302
+        assert response.url == f'{reverse("instaclone-post_view", kwargs={"pk": post.id})}'
+        post.refresh_from_db()
+        assert post.caption == new_caption
+        logger.info(f'The updated caption is {post.caption}')
+
+    @pytest.mark.skip
+    def test_edit_by_creator_invalid_data(self):
+        post = mixer.blend(Post)
+        logger.info(f'Mixer created post is {post}')
+        logger.info(f'Mixer created post has id {post.id}')
+        logger.info(f'Mixer created post has caption {post.caption}')
+        logger.info(f'Mixer created post has creator {post.creator}')
+        logger.debug(f'All posts queryset is {Post.objects.all()}')
+
+        new_caption = random_string(513)
+        logger.debug(f'Randomly generated caption has length {len(new_caption)}')
+        data = {
+            'caption': new_caption,
+        }
+        request = RequestFactory().post(
+            reverse('instaclone-edit_post_view', kwargs={'pk': post.id}),
+            data=data,
+        )
+        request.user = post.creator
+        response = views.edit_post_view(request, pk=post.id)
+        assert response.status_code == 302
+        assert response.url == f'{reverse("instaclone-post_view", kwargs={"pk": post.id})}'
