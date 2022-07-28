7983165a250a6d5415ae77abeed118094586e1a9
Advaith H L <niceadvaith@gmail.com>
2022-07-28 17:59:34 +0530

    Make gunicorn reload on DEBUG mode

--- instaclone/gunicorn.conf.py
+++ instaclone/gunicorn.conf.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from os import getenv as os_getenv
 
 
 bind = '0.0.0.0:8000'
@@ -6,3 +7,7 @@ workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = 'instaclone.wsgi:application'
 
 loglevel = 'info'
+
+if os_getenv('INSTACLONE_DJANGO_DEBUG_MODE') == 'debug':
+    loglevel = 'debug'
+    reload = True
