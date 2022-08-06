c65c530700560125247889b18876a091b630b5ab
Advaith H L <niceadvaith@gmail.com>
2022-08-06 17:25:01 +0530

    Remove unnecessary files from containers

--- instaclone/celery.docker-entrypoint.sh
+++ instaclone/celery.docker-entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+# Remove Python requirements.txt.
+rm requirements.txt
+
+# Remove Docker entrypoint scripts.
+rm *docker-entrypoint.sh
+
+# Remove gunicorn config file.
+rm gunicorn.conf.py
+
 # Execute celery as the PID 1 process.
 exec celery -A instaclone worker -l INFO
--- instaclone/docker-entrypoint.sh
+++ instaclone/docker-entrypoint.sh
@@ -6,5 +6,11 @@ python manage.py migrate --no-input
 # Collect static files to STATIC_ROOT of django.
 python manage.py collectstatic --no-input
 
+# Remove Python requirements.txt.
+rm requirements.txt
+
+# Remove Docker entrypoint scripts.
+rm *docker-entrypoint.sh
+
 # Execute gunicorn as the PID 1 process.
 exec gunicorn
