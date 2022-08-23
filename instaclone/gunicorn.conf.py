32a34938ad079471fb058446cf98da80ff7e2102
Advaith H L <niceadvaith@gmail.com>
2022-08-23 10:13:27 +0530

    Revert "Make gunicorn reload on DEBUG mode"
    This reverts commit 7983165a250a6d5415ae77abeed118094586e1a9.

--- instaclone/gunicorn.conf.py
+++ instaclone/gunicorn.conf.py
@@ -1,5 +1,4 @@
 import multiprocessing
-from os import getenv as os_getenv
 
 
 bind = '0.0.0.0:8000'
@@ -7,7 +6,3 @@ workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = 'instaclone.wsgi:application'
 
 loglevel = 'info'
-
-if os_getenv('INSTACLONE_DJANGO_DEBUG_MODE') == 'debug':
-    loglevel = 'debug'
-    reload = True
